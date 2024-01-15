d45f7c047d3c2ec392d7f38aa04f5e4b2ca6f96e
Md. Raju Ahmed <rajuahmad6789@gmail.com>
2024-01-15 23:53:39 +0600

    fixed layout for all pages

--- src/Pages/Dashboard/Customer/Index.jsx
+++ src/Pages/Dashboard/Customer/Index.jsx
@@ -5,22 +5,16 @@ import CustomerList from './List/CustomerList';
 import CustomersCreate from './Create/Create';
 import CustomersShow from './Show/Show';
 import CustomersEdit from './Edit/Edit';
-import Sidebar from '../MixRecipes/Sidebar/Sidebar';
 
 const Customers = () => {
   return (
-    <div className="page-content">
-      <div className="page-sidebar">
-        <Sidebar />
-      </div>
-      <div className="page-component">
-        <Routes>
-          <Route path="/" element={<CustomerList />} />
-          <Route path="/create" element={<CustomersCreate />} />
-          <Route path="/show/:id" element={<CustomersShow />} />
-          <Route path="/edit/:id" element={<CustomersEdit />} />
-        </Routes>
-      </div>
+    <div className="page-component">
+      <Routes>
+        <Route path="/" element={<CustomerList />} />
+        <Route path="/create" element={<CustomersCreate />} />
+        <Route path="/show/:id" element={<CustomersShow />} />
+        <Route path="/edit/:id" element={<CustomersEdit />} />
+      </Routes>
     </div>
   );
 };
