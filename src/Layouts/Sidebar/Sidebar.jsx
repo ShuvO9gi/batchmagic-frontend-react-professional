9703b7560478ecfbb3e3deeea3d7789600df4273
Md. Raju Ahmed <rajuahmad6789@gmail.com>
2024-02-02 02:22:30 +0600

    fix sidebar open close anomalies

--- src/Layouts/Sidebar/Sidebar.jsx
+++ src/Layouts/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 import { sidebarMenu } from './sidebar-components';
 import { Link } from 'react-router-dom';
@@ -18,11 +18,25 @@ const Sidebar = () => {
     ) {
       return true;
     }
+
+    // Check if any submenu item is active
+    if (menuItem.submenu) {
+      return menuItem.submenu.some((subItem) =>
+        currentPath.includes(subItem.link),
+      );
+    }
+
     return currentPath.includes(menuItem.link);
   };
 
   const isActiveSubmenu = (submenuItem) => {
     const data = currentPath.includes(submenuItem.link);
+    // If the submenu item is active, set the parent sidebar item as open
+    useEffect(() => {
+      if (data) {
+        setOpenSubMenu(submenuItem.parentLink || '');
+      }
+    }, [data, setOpenSubMenu, submenuItem.parentLink]);
     return data;
   };
 
