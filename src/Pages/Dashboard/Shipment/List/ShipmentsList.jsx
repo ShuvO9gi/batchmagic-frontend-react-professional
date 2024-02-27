000e10cad93d5e73dcda7fbe141258e13f6e5360
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-02-27 12:32:16 +0600

    style added

--- src/Pages/Dashboard/Shipment/List/ShipmentsList.jsx
+++ src/Pages/Dashboard/Shipment/List/ShipmentsList.jsx
@@ -107,7 +107,12 @@ export default function Index() {
 
   return (
     <div>
-      <h1 className="text-center my-64 list-header">Shipments Order</h1>
+      <h1
+        className="text-center my-64 list-header"
+        style={{ fontSize: '30px' }}
+      >
+        Shipments Order
+      </h1>
       <DataTables
         columns={columns}
         data={memoizedData}
