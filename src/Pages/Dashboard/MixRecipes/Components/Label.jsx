2e1aa5e6d3b7cdc7a08c42bc5b92b4afdcf4196f
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-08-01 00:40:48 +0600

    ui updated

--- src/Pages/Dashboard/MixRecipes/Components/Label.jsx
+++ src/Pages/Dashboard/MixRecipes/Components/Label.jsx
@@ -254,25 +254,27 @@ const Label = ({ onClose, batchTemplateID }) => {
       <div className="modal-body-recipes modal-body-recipes-label ">
         <div className="d-flex justify-content-between align-items-baseline modal-header-recipes list-header">
           <p className="fw-bold fs-6">Label</p>
-          {currentLabel?.id && (
-            <button
-              type="button"
-              className="btn btn-orange create-create-btn-customized me-2"
-              style={{ paddingTop: 0, paddingBottom: 0, width: 'auto' }}
-              onClick={() => setLabelValue(currentLabel?.id)}
-            >
-              UPLOAD
-            </button>
-          )}
-          <div>
-            <img
-              onClick={() => {
-                onClose();
-              }}
-              className="modal-close"
-              src={close}
-              alt=""
-            />
+          <div className="d-flex">
+            {currentLabel?.id && (
+              <button
+                type="button"
+                className="btn btn-orange create-create-btn-customized me-2"
+                style={{ paddingTop: 0, paddingBottom: 0, width: 'auto' }}
+                onClick={() => setLabelValue(currentLabel?.id)}
+              >
+                UPLOAD
+              </button>
+            )}
+            <div>
+              <img
+                onClick={() => {
+                  onClose();
+                }}
+                className="modal-close"
+                src={close}
+                alt=""
+              />
+            </div>
           </div>
         </div>
         <hr className="my-0" />
